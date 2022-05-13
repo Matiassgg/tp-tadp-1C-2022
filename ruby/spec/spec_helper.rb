861b29d698ac574e029812ea389ef9c58f02d4c8
Ernesto Bossi <bossi.ernestog@gmail.com>
2022-05-12 21:11:12 -0300

    add pry to debug

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,3 +1,8 @@
 require 'rspec'
+require 'pry'
+
+#fw contratos
 require_relative '../lib/contrato.rb'
 
+#fixtures
+require_relative 'fixture/mi_clase'
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,8 +1,10 @@
-require_relative 'fixture/mi_clase'
+require_relative 'spec_helper'
 
 describe 'suite de tests para 1° requerimiento' do
     it 'Se mandan los mensajes de la ejecucion' do
         clase_test = MiClase.new
+        # Debugging
+        # binding.pry
         expect(clase_test.mensaje_1).to be 5
         expect(clase_test.mensaje_2).to be 3
     end

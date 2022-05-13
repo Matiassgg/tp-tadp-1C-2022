91e59d86a169ba44248b465ddd13f0e10d036002
Ernesto Bossi <bossi.ernestog@gmail.com>
2022-05-12 21:02:57 -0300

    correcciones 12.05.22

---
+++ ruby/spec/fixture/mi_clase.rb
@@ -0,0 +1,30 @@
+require_relative '../../lib/contrato.rb'
+
+class MiClase
+  include Contratos
+
+  def mensaje_1
+    puts "mensaje_1"
+    5
+  end
+
+  def mensaje_2
+    puts "mensaje_2"
+    3
+  end
+
+  before_and_after_each_call(
+    proc { puts "Entré a un mensaje 1" },
+    proc { puts "Salí de un mensaje 1" }
+  )
+
+  before_and_after_each_call(
+    proc { puts "Entré a un mensaje 2" },
+    proc { puts "Salí de un mensaje 2" }
+  )
+
+  # Con el wrapper no es necesario
+  def self.method_added(nombre)
+    puts "se agrega el metodo #{nombre}"
+  end
+end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,13 +1,10 @@
-require 'rspec'
-require_relative '../lib/Contrato.rb'
+require_relative 'fixture/mi_clase'
 
 describe 'suite de tests para 1° requerimiento' do
-
     it 'Se mandan los mensajes de la ejecucion' do
         clase_test = MiClase.new
         expect(clase_test.mensaje_1).to be 5
         expect(clase_test.mensaje_2).to be 3
     end
-
 end
 
